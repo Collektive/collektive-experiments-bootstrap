213a2e0948d8993b996290123a3aaf597575f363
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-28 16:27:12 +0200

    style: fix according to ktlint

--- src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
+++ src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
@@ -1,9 +1,9 @@
 package it.unibo.collektive.examples.gradient
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
+import it.unibo.collektive.aggregate.Field
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.share
-import it.unibo.collektive.aggregate.Field
 import it.unibo.collektive.stdlib.doubles.FieldedDoubles.plus
 import it.unibo.collektive.stdlib.fields.minValue
 import kotlin.Double.Companion.POSITIVE_INFINITY
@@ -25,9 +25,10 @@ fun Aggregate<Int>.gradient(
 /**
  * The entrypoint of the simulation running a gradient.
  */
-fun Aggregate<Int>.gradientEntrypoint(distanceSensor: CollektiveDevice<*>): Double = with(distanceSensor) {
-    return gradient(
-        distances = distances(),
-        source = localId == 0,
-    )
-}
+fun Aggregate<Int>.gradientEntrypoint(distanceSensor: CollektiveDevice<*>): Double =
+    with(distanceSensor) {
+        return gradient(
+            distances = distances(),
+            source = localId == 0,
+        )
+    }
