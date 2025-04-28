821f4fe33dde90f83cb2b6c65b7bd41d03bb9559
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-28 15:48:22 +0200

    fix: adapt to collektive v23.0.4

--- src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
+++ src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
@@ -3,25 +3,31 @@ package it.unibo.collektive.examples.gradient
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.share
-import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
-import it.unibo.collektive.field.operations.min
+import it.unibo.collektive.aggregate.Field
 import it.unibo.collektive.stdlib.doubles.FieldedDoubles.plus
+import it.unibo.collektive.stdlib.fields.minValue
 import kotlin.Double.Companion.POSITIVE_INFINITY
 
 /**
  * Extension function to evaluate the gradient in an [Aggregate] context.
  */
-fun Aggregate<Int>.gradient(distanceSensor: CollektiveDevice<*>, source: Boolean): Double =
+fun Aggregate<Int>.gradient(
+    distances: Field<Int, Double>,
+    source: Boolean,
+): Double =
     share(POSITIVE_INFINITY) {
-        val dist = with(distanceSensor) { distances() }
         when {
             source -> 0.0
-            else -> (it + dist).min(POSITIVE_INFINITY)
+            else -> (it + distances).minValue(POSITIVE_INFINITY)
         }
     }
 
 /**
  * The entrypoint of the simulation running a gradient.
  */
-fun Aggregate<Int>.gradientEntrypoint(env: EnvironmentVariables, distanceSensor: CollektiveDevice<*>): Double =
-    gradient(distanceSensor, env["source"])
+fun Aggregate<Int>.gradientEntrypoint(distanceSensor: CollektiveDevice<*>): Double = with(distanceSensor) {
+    return gradient(
+        distances = distances(),
+        source = localId == 0,
+    )
+}
